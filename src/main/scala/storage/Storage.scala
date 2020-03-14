d3f48a2113d939111e3bc8bdc4a702bf51040d6a
Brian <b.o.s.planje@student.tudelft.nl>
2020-03-14 18:08:25 +0100

    Add a basic storage class implementing the interface with tests

--- src/main/scala/storage/Storage.scala
+++ src/main/scala/storage/Storage.scala
@@ -1,19 +1,71 @@
 package storage
 
+import ujson.Value.Value
+
 class Storage {
 
-  var storage: Map[Int, String] = _
+  var storage: Map[Int, String] = Map()
 
-  def init(): Unit = {
-    storage = Map()
+  def query(objectId: Int, options: Option[List[String]] = None): Option[String] = {
+    // Get entry from source.
+    val databaseEntry: Option[String] = storage.get(objectId)
+
+    databaseEntry match {
+      case Some(jsonStringAllFields) =>
+        // Read the string entry into JSON.
+        val json: Value = ujson.read(jsonStringAllFields).obj
+        val jsonObject = json.obj
+
+        // Stop if the JSON is not an object.
+        if (!json.isInstanceOf[ujson.Obj]) {
+          return None
+        }
+
+        // Apply options (fields) to return.
+        options match {
+          case Some(options) =>
+            // Fields given, return only requested fields.
+            // TODO: make this more efficient
+            val filteredObj = jsonObject.filter(x => options.contains(x._1))
+            val jsonString = ujson.write(filteredObj)
+            Some(jsonString)
+          case None =>
+            // No options given, return all fields.
+            val jsonString = ujson.write(jsonObject)
+            Some(jsonString)
+        }
+      case None => None
+    }
   }
 
-  def query(objectId: Int, options: Option[List[String]] = None): String = {
-    ""
-  }
+  def update(objectId: Int, newValue: String, options: Option[List[String]] = None): Option[String] = {
 
-  def update(objectId: Int, newValue: String, options: Option[List[String]] = None): String = {
-    ""
+    // Get entry from source.
+    val databaseEntry = storage.get(objectId)
+
+    // Parse object that the client sent.
+    val jsonObject = ujson.read(newValue)
+
+    // Stop if the JSON is not an object.
+    if (!jsonObject.isInstanceOf[ujson.Obj]) {
+      return None
+    }
+
+    databaseEntry match {
+      case Some(previousValue) =>
+        // Object present, update.
+        // TODO: interpret options and non-deterministic update
+        val jsonStringUpdated = ujson.write(jsonObject)
+        storage = storage.updated(objectId, jsonStringUpdated)
+
+        Some(jsonStringUpdated)
+      case None =>
+        // No object present, create.
+        val jsonStringUpdated = ujson.write(jsonObject)
+        storage = storage.updated(objectId, jsonStringUpdated)
+
+        Some(jsonStringUpdated)
+    }
   }
 
 
--- src/test/scala/storage/StorageTest.scala
+++ src/test/scala/storage/StorageTest.scala
@@ -4,7 +4,80 @@ import org.scalatest._
 
 class StorageTest extends FunSuite {
 
-  test("should initialize") {
-    val storage = new Storage()
+  val rawJson: String =
+    """{"name":"Watership Down","location":{"lat":51.235685,"long":-1.309197},"residents":[{"name":"Fiver","age":4,"role":null},{"name":"Bigwig","age":6,"role":"Owsla"}]}"""
+
+  val rawJsonNameFieldOnly: String =
+    """{"name":"Watership Down"}"""
+
+  val listJson: String =
+    """[1, 2, 3, 4, 5]"""
+
+  test("should initialize without errors") {
+    new Storage()
   }
+
+  test("should update object and receive it back successfully") {
+    val storage = new Storage()
+    val updateResult = storage.update(1, rawJson)
+
+    updateResult match {
+      case Some(jsonBack) =>
+        assert(jsonBack == rawJson)
+      case None => fail()
+    }
+  }
+
+  test("should update object and receive it back successfully after query") {
+    val storage = new Storage()
+    storage.update(1, rawJson)
+
+    val query = storage.query(1)
+
+    query match {
+      case Some(jsonBack) =>
+        assert(jsonBack == rawJson)
+      case None => fail()
+    }
+  }
+
+  test("should only receive the given field after query") {
+    val storage = new Storage()
+    storage.update(1, rawJson)
+
+    val options = Some(List("name"))
+    val query = storage.query(1, options)
+
+    query match {
+      case Some(jsonBack) =>
+        assert(jsonBack == rawJsonNameFieldOnly)
+      case None => fail()
+    }
+  }
+
+  test("should update two objects which are both defined") {
+    val storage = new Storage()
+    val updateResult = storage.update(1, rawJson)
+    val updateResult2 = storage.update(2, rawJson)
+
+    (updateResult, updateResult2) match {
+      case (Some(jsonBack), Some(jsonBack2)) =>
+        assert(jsonBack == rawJson && jsonBack2 == rawJson)
+      case _ => fail()
+    }
+  }
+
+  test("should deny any non object updates") {
+    val storage = new Storage()
+    val updatedResult = storage.update(1, listJson)
+
+    updatedResult match {
+      case Some(a) =>
+        println(a)
+        fail()
+      case None => assert(true)
+    }
+  }
+
 }
+
