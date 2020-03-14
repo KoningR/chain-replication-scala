1690d7b0ea7cc255cb56e124c11082baebd35c38
KoningR <R.M.Koning@student.tudelft.nl>
2020-03-14 13:10:10 +0100

    Move message handling to separate functions for easier debugging and merging.

--- src/main/scala/actors/Client.scala
+++ src/main/scala/actors/Client.scala
@@ -1,59 +1,61 @@
 package actors
 
-import actors.MasterService.{MasterServiceReceivable, PingMasterService, RequestChainInfo}
-import actors.Server.{ServerReceivable, Update}
-import akka.actor.typed.scaladsl.Behaviors
+import actors.MasterService.RequestChainInfo
+import actors.Server.{Query, ServerReceivable, Update}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.scaladsl.adapter._
 import akka.actor.typed.{ActorRef, Behavior}
 
 object Client {
 
     sealed trait ClientReceivable
-
     final case class InitClient(remoteMasterServicePath: String) extends ClientReceivable
-
-    final case class PongClient(masterService: ActorRef[MasterServiceReceivable]) extends ClientReceivable
-
     final case class ChainInfoResponse(head: ActorRef[ServerReceivable], tail: ActorRef[ServerReceivable]) extends ClientReceivable
+    final case class QueryResponse(objId: Int, queryResult: String) extends ClientReceivable
+    final case class UpdateResponse(objId: Int, newValue: String) extends ClientReceivable
 
-    final case class QueryResponse(objId: Int, obj: String) extends ClientReceivable
+    private var head: ActorRef[ServerReceivable] = _
+    private var tail: ActorRef[ServerReceivable] = _
 
-    final case class UpdateResponse(objId: Int) extends ClientReceivable
+    def initClient(context: ActorContext[ClientReceivable], message: ClientReceivable, remoteMasterServicePath: String): Behavior[ClientReceivable] = {
+        val masterService = context.toClassic.actorSelection(remoteMasterServicePath)
 
-    var head: ActorRef[ServerReceivable] = _
-    var tail: ActorRef[ServerReceivable] = _
+        masterService ! RequestChainInfo(context.self)
 
-    def apply(): Behavior[ClientReceivable] = Behaviors.receive {
-        (context, message) =>
-            message match {
-                case InitClient(remoteMasterServicePath) =>
-                    val masterService = context.toClassic.actorSelection(remoteMasterServicePath)
-                    context.log.info("Client: will try to discover masterService {} by pinging it.", masterService.pathString)
-                    masterService ! PingMasterService(context.self)
-                    Behaviors.same
-                case PongClient(masterService) =>
-                    context.log.info("Client: has discovered server! Requesting chain info.")
-                    masterService ! RequestChainInfo(context.self)
-                    Behaviors.same
-                case ChainInfoResponse(head, tail) =>
-                    context.log.info("Client: received a ChainInfoResponse, head: {}, tail: {}", head.path, tail.path)
-                    this.head = head
-                    this.tail = tail
-                    this.head ! Update(1, "New object", context.self)
-                    Behaviors.same
-                case QueryResponse(objId, obj) =>
-                    context.log.info("Client: received a QueryResponse for objId {} = {}", objId, obj)
-                    Behaviors.same
-                case UpdateResponse(objId) =>
-                    context.log.info("Client: received a UpdateResponse for objId {}", objId)
-                    Behaviors.same
-            }
+        context.log.info("Client: will try to get the chain's head and tail from the masterService {}.", masterService.pathString)
+        Behaviors.same
     }
 
-}
+    def chainInfoResponse(context: ActorContext[ClientReceivable], message: ClientReceivable, head: ActorRef[ServerReceivable], tail: ActorRef[ServerReceivable]): Behavior[ClientReceivable] = {
+        this.head = head
+        this.tail = tail
 
-// Master service should ping servers and remove if they are down
-// Client should be able to accept console input and do a query
-// Servers should pass message through the chain using ACKNOWLEDGEMENT as specified in paper
-// Servers should update requests and pass updated state
-// Servers should be added to the end of the chain and updated
+        this.head ! Query(1, context.self)
+
+        context.log.info("Client: received a ChainInfoResponse, head: {}, tail: {}", head.path, tail.path)
+        Behaviors.same
+    }
+
+    def queryResponse(context: ActorContext[ClientReceivable], message: ClientReceivable, objId: Int, queryResult: String): Behavior[ClientReceivable] = {
+        this.head ! Update(1, "New object", context.self)
+
+        context.log.info("Client: received a QueryResponse for objId {} = {}", objId, queryResult)
+        Behaviors.same
+    }
+
+    def updateResponse(context: ActorContext[ClientReceivable], message: ClientReceivable, objId: Int, newValue: String): Behavior[ClientReceivable] = {
+        context.log.info("Client: received a UpdateResponse for objId {}, new value is {}", objId, newValue)
+        Behaviors.same
+    }
+
+    def apply(): Behavior[ClientReceivable] = Behaviors.receive {
+        (context, message) => {
+            message match {
+                case InitClient(remoteMasterServicePath) => initClient(context, message, remoteMasterServicePath)
+                case ChainInfoResponse(head, tail) => chainInfoResponse(context, message, head, tail)
+                case QueryResponse(objId, queryResult) => queryResponse(context, message, objId, queryResult)
+                case UpdateResponse(objId, newValue) => updateResponse(context, message, objId, newValue)
+            }
+        }
+    }
+}
--- src/main/scala/actors/MasterService.scala
+++ src/main/scala/actors/MasterService.scala
@@ -1,46 +1,47 @@
 package actors
 
-import actors.Client.{ChainInfoResponse, ClientReceivable, PongClient}
+import actors.Client.{ChainInfoResponse, ClientReceivable}
 import actors.Server.{RegisteredServer, ServerReceivable}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 
 object MasterService {
 
     sealed trait MasterServiceReceivable
-
     final case class InitMasterService() extends MasterServiceReceivable
-
-    final case class PingMasterService(replyTo: ActorRef[ClientReceivable]) extends MasterServiceReceivable
-
     final case class RequestChainInfo(replyTo: ActorRef[ClientReceivable]) extends MasterServiceReceivable
-
     final case class RegisterServer(replyTo: ActorRef[ServerReceivable]) extends MasterServiceReceivable
 
-    var head: ActorRef[ServerReceivable] = _
-    var tail: ActorRef[ServerReceivable] = _
+    private var chain = List[ActorRef[ServerReceivable]]()
 
     def apply(): Behavior[MasterServiceReceivable] = Behaviors.receive {
-        (context, message) =>
+        (context, message) => {
             message match {
-                case InitMasterService() =>
-                    context.log.info("MasterService: master service is initialized.")
-                    Behaviors.same
-                case PingMasterService(replyTo) =>
-                    context.log.info("MasterService: received a register request from a server, sending response.")
-                    replyTo ! PongClient(context.self)
-                    Behaviors.same
-                case RegisterServer(replyTo) =>
-                    context.log.info("MasterService: received a register request from a server, sending response.")
-                    this.head = replyTo
-                    this.tail = replyTo
-                    replyTo ! RegisteredServer(context.self)
-                    Behaviors.same
-                case RequestChainInfo(replyTo) =>
-                    context.log.info("MasterService: received a chain request from a client, sending info.")
-                    replyTo ! ChainInfoResponse(this.head, this.tail)
-                    Behaviors.same
+                case InitMasterService() => initMasterService(context, message)
+                case RegisterServer(replyTo) => registerServer(context, message, replyTo)
+                case RequestChainInfo(replyTo) => requestChainInfo(context, message, replyTo)
             }
+        }
     }
 
+    def initMasterService(context: ActorContext[MasterServiceReceivable], message: MasterServiceReceivable): Behavior[MasterServiceReceivable] = {
+        context.log.info("MasterService: master service is initialized.")
+        Behaviors.same
+    }
+
+    def registerServer(context: ActorContext[MasterServiceReceivable], message: MasterServiceReceivable, replyTo: ActorRef[ServerReceivable]): Behavior[MasterServiceReceivable] = {
+        chain = replyTo :: chain
+
+        replyTo ! RegisteredServer(context.self, replyTo, replyTo, isHead = true, isTail = true)
+
+        context.log.info("MasterService: received a register request from a server, sent response.")
+        Behaviors.same
+    }
+
+    def requestChainInfo(context: ActorContext[MasterServiceReceivable], message: MasterServiceReceivable, replyTo: ActorRef[ClientReceivable]): Behavior[MasterServiceReceivable] = {
+        replyTo ! ChainInfoResponse(chain.head, chain.last)
+
+        context.log.info("MasterService: received a chain request from a client, sent info.")
+        Behaviors.same
+    }
 }
