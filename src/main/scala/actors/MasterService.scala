89015db80a9c91101ceb1f15fcee8c9ee1877146
MKuijpers <mika.kuijpers@live.nl>
2020-03-15 14:39:06 +0100

    Add new servers to the chain in master, update server with new position

--- src/main/scala/actors/MasterService.scala
+++ src/main/scala/actors/MasterService.scala
@@ -1,7 +1,7 @@
 package actors
 
 import actors.Client.{ChainInfoResponse, ClientReceivable}
-import actors.Server.{RegisteredServer, ServerReceivable}
+import actors.Server.{ChainPositionUpdate, RegisteredServer, ServerReceivable}
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.actor.typed.{ActorRef, Behavior}
 import communication.JsonSerializable
@@ -12,6 +12,7 @@ object MasterService {
     final case class InitMasterService() extends MasterServiceReceivable
     final case class RequestChainInfo(replyTo: ActorRef[ClientReceivable]) extends MasterServiceReceivable
     final case class RegisterServer(replyTo: ActorRef[ServerReceivable]) extends MasterServiceReceivable
+    final case class RequestChainPosition(replyTo: ActorRef[ServerReceivable]) extends MasterServiceReceivable
 
     private var chain = List[ActorRef[ServerReceivable]]()
 
@@ -21,6 +22,7 @@ object MasterService {
                 case InitMasterService() => initMasterService(context, message)
                 case RegisterServer(replyTo) => registerServer(context, message, replyTo)
                 case RequestChainInfo(replyTo) => requestChainInfo(context, message, replyTo)
+                case RequestChainPosition(replyTo) => requestChainPosition(context, message, replyTo)
             }
         }
     }
@@ -31,9 +33,13 @@ object MasterService {
     }
 
     def registerServer(context: ActorContext[MasterServiceReceivable], message: MasterServiceReceivable, replyTo: ActorRef[ServerReceivable]): Behavior[MasterServiceReceivable] = {
+        // Always add new server to the head of the chain
         chain = replyTo :: chain
 
-        replyTo ! RegisteredServer(context.self, replyTo, replyTo, isHead = true, isTail = true)
+        replyTo ! RegisteredServer(context.self)
+
+        // TODO: can be optimized to only send update to the neighbours of this server
+        chain.zipWithIndex.foreach{ case (server, index) => chainPositionUpdate(context, server, index) }
 
         context.log.info("MasterService: received a register request from a server, sent response.")
         Behaviors.same
@@ -44,5 +50,23 @@ object MasterService {
 
         context.log.info("MasterService: received a chain request from a client, sent info.")
         Behaviors.same
+    }
+
+    def requestChainPosition(context: ActorContext[MasterServiceReceivable], message: MasterServiceReceivable, replyTo: ActorRef[ServerReceivable]): Behavior[MasterServiceReceivable] = {
+        val index = chain.indexOf(replyTo)
+        chainPositionUpdate(context, replyTo, index)
+
+        context.log.info("MasterService: received a chain position request from a server, sent position.")
+        Behaviors.same
+    }
+
+    def chainPositionUpdate(context: ActorContext[MasterServiceReceivable],
+                                 server: ActorRef[ServerReceivable], index: Int): Unit = {
+        val isHead = index == 0
+        val isTail = index == chain.length - 1
+        val previous = chain(Math.max(index - 1, 0))
+        val next = chain(Math.min(index + 1, chain.length - 1))
+        context.log.info("MasterService sent {} chain position: isHead: {}, isTail: {}, previous: {} and next: {}", server, isHead, isTail, previous, next)
+        server ! ChainPositionUpdate(isHead, isTail, previous, next)
     }
 }
