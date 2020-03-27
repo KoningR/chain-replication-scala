f820e9d630904bd8c5ce41ff529982ed1a68fe89
MKuijpers <mika.kuijpers@live.nl>
2020-03-27 14:56:43 +0100

    Move CLI to ClientInitializer

--- src/main/scala/initializers/ClientInitializer.scala
+++ src/main/scala/initializers/ClientInitializer.scala
@@ -1,11 +1,13 @@
 package initializers
 
 import actors.Client
-import actors.Client.InitClient
+import actors.Client.{CallQuery, CallUpdate, InitClient}
 import akka.NotUsed
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior, Terminated}
 import com.typesafe.config.ConfigFactory
+
+import scala.io.StdIn
 
 object ClientInitializer {
 
@@ -16,6 +18,49 @@ object ClientInitializer {
             context => {
                 val client = context.spawn(Client(), "CRS")
                 client ! InitClient(MASTER_SERVICE_PATH)
+
+                var input = StdIn.readLine("\n")
+                while (!input.equals("quit") || !input.equals("q")) {
+                    val command: String = input.split(" ").map(_.trim).toList(0)
+
+                    command match {
+                        case "query" =>
+                            println("Query command called.")
+                            val inputList: List[String] = input.split(" ").map(_.trim).filter(_.length > 0).toList
+
+                            inputList match {
+                                case _ :: objId :: options =>
+                                    println("   Object ID: " + objId)
+                                    println("   Options: ")
+                                    options.foreach(println)
+                                    val optionsParameter = options match {
+                                        case Nil => None
+                                        case list => Some(list)
+                                    }
+                                    client ! CallQuery(objId.toInt, optionsParameter)
+                            }
+                        case "update" =>
+                            println("Update command called.")
+                            val newObj = input.substring(input.indexOf("{"), input.lastIndexOf("}") + 1)
+                            val inputWithoutObject = input.replace(newObj, "")
+                            val inputWithoutObjectList: List[String] = inputWithoutObject.split(" ").map(_.trim).filter(_.length > 0).toList
+
+                            inputWithoutObjectList match {
+                                case _ :: objId :: options =>
+                                    println("   Object ID : " + objId)
+                                    println("   New Object: " + newObj)
+                                    println("   Options: ")
+                                    options.foreach(println)
+                                    val optionsParameter = options match {
+                                        case Nil => None
+                                        case list => Some(list)
+                                    }
+                                    client ! CallUpdate(objId.toInt, newObj, optionsParameter)
+                            }
+                        case _ => println("Command was not valid.")
+                    }
+                    input = StdIn.readLine("\n")
+                }
 
                 Behaviors.receiveSignal {
                     case (_, Terminated(_)) =>
